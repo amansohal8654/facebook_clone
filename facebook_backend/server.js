43f59fedb6b5b1da7daec714c7442cb157553549
amansohal8654 <amansohal8654@gmail.com>
2020-10-15 13:53:50 +1300

    final push

---
+++ facebook_backend/server.js
@@ -0,0 +1,147 @@
+import express from "express"
+import mongoose from  "mongoose"
+import cors from "cors"
+import multer from "multer"
+import GridFsStorage from  "multer-gridfs-storage"
+import Grid from "gridfs-stream"
+import bodyParser from 'body-parser'
+import path from 'path'
+import Pusher from 'pusher'
+
+import mongoPosts from './postModel.js'
+
+Grid.mongo = mongoose.mongo
+
+//app config
+const app = express()
+const port = process.env.PORT || 9000
+
+const pusher = new Pusher({
+    appId: '1089909',
+    key: '48b5f5130bcca7f2f281',
+    secret: '60988a5be196a1e44717',
+    cluster: 'ap4',
+    useTLS: true
+  });
+
+//middlewares
+app.use(bodyParser.json());
+app.use(cors())
+
+//do config mongodb+srv://fb_clinet:<password>@cluster0.vaxet.mongodb.net/<dbname>?retryWrites=true&w=majority
+const mongoURI = "mongodb+srv://fb_clinet:T2vMv7x0LDBamT2g@cluster0.vaxet.mongodb.net/facebook-db?retryWrites=true&w=majority"
+
+const conn = mongoose.createConnection(mongoURI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.once('open', () =>{
+    console.log('DB Connected')
+
+    const changeStream = mongoose.connection.collection('posts').watch()
+
+    changeStream.on('change', (change) => {
+        console.log(change)
+
+        if(change.operationType === 'insert') {
+            console.log('Triggering Pusher')
+
+            pusher.trigger('posts', 'inserted', {
+                change: change
+            })
+
+        } else {
+            console.log('Error triggering Pusher')
+        }
+    })
+})
+
+
+let gfs
+
+conn.once('open', () => {
+    console.log('DB Connected')
+
+    gfs = Grid(conn.db, mongoose.mongo)
+    gfs.collection('images')
+})
+
+const storage = new GridFsStorage({
+    url: mongoURI,
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+            const filename = `image-${Date.now()}${path.extname(file.originalname)}`
+
+            const fileInfo = {
+                filename: filename,
+                bucketName: 'images'
+            }
+
+            resolve(fileInfo);
+        });
+    }
+});
+
+
+mongoose.connect(mongoURI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const upload = multer({storage})
+
+//api routes
+app.get('/', (req, res) => 
+res.status(200).send('hello Aman'))
+
+app.post('/upload/image', upload.single('file'), (req, res) => {
+    res.status(201).send(req.file)
+})
+
+app.post('/upload/post', (req, res) => {
+    const dbPost = req.body
+
+    mongoPosts.create(dbPost, (err, data) => {
+        if(err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    })
+})
+
+app.get('/retrieve/post', (req, res) => {
+    mongoPosts.find((err, data) =>{
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            data.sort((b, a) => {
+                return a.timestamp - b.timestamp;
+            })
+
+            res.status(200).send(data)
+        }
+    })
+})
+
+app.get('/retrieve/images/single', (req, res) => {
+    gfs.files.findOne({filename: req.query.name}, (err, file) => {
+        if (err) {
+            req.status(500).send(err)
+        } else {
+            if (!file || file.length === 0) {
+                res.status(404).json({err: 'file not found'})
+            } else {
+                const readstream = gfs.createReadStream(file.filename);
+                readstream.pipe(res);
+            }
+    
+        }
+    })
+})
+
+// listen
+app.listen(port, () => console.log(`listening on localhost: ${port}`))
---
+++ facebook_clone/build/precache-manifest.005a7a5e4528d68994af4ae124eb6d03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd0a3fd750d102aa235b4c70bf8babd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b03bcf939a1ae951e9c7",
+    "url": "/static/css/main.535d8467.chunk.css"
+  },
+  {
+    "revision": "35fa7aa25a08cf41eb37",
+    "url": "/static/js/2.e66e4ad7.chunk.js"
+  },
+  {
+    "revision": "0299ef914f37a5dcf98639bd938f0de1",
+    "url": "/static/js/2.e66e4ad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b03bcf939a1ae951e9c7",
+    "url": "/static/js/main.04141afb.chunk.js"
+  },
+  {
+    "revision": "14f2ee8d28fceb42b6fd",
+    "url": "/static/js/runtime-main.10c5eb34.js"
+  }
+]);
